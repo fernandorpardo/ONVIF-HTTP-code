3fa2affa6c647987e7c9db3192f10de7b8dcbcb9
Fernando R <38065602+fernandorpardo@users.noreply.github.com>
2023-09-21 21:12:41 +0200

    Delete debug.h

--- debug.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef _HADEBUG_H_
-#define _HADEBUG_H_
-
-//#define WS_DEBUG "/var/www/ramdisk/wsdebug.log"
-//void filetrace(char *str, bool do_ts);
-void trace2file( char const*filename, char *str, bool do_ts=true);
-#endif
-// END OF FILE
